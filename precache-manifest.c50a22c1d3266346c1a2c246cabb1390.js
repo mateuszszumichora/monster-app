011dad9185742f01879e4a683bf196073c643fe0
Mateusz Szumichora <mateuszszumichora@gmail.com>
2019-08-20 19:06:06 +0200

    Updates

---
+++ precache-manifest.c50a22c1d3266346c1a2c246cabb1390.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "194b965b0e47f35cff0c22c4adde4109",
+    "url": "/monster-app/index.html"
+  },
+  {
+    "revision": "2e72adc706e3e2de0b30",
+    "url": "/monster-app/static/css/main.0386dc6a.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monster-app/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "2e72adc706e3e2de0b30",
+    "url": "/monster-app/static/js/main.3e9684a6.chunk.js"
+  },
+  {
+    "revision": "c5d2758b672667244f5a",
+    "url": "/monster-app/static/js/runtime~main.ac074dc3.js"
+  }
+]);
